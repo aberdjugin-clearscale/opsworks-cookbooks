9ff465a4bbdcdcd3288039c500e6c6a11cf0746a
alexey.berdjugin <alexey.berdjugin@clearscale.net>
2016-08-09 10:55:35 +0700

    First commit

--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -3,5 +3,4 @@ maintainer       'Clearscale'
 maintainer_email ''
 license          'All rights reserved'
 description      'Configures wordpress'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
