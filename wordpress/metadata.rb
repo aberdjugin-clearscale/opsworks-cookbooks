fec095eb6a58fa76ecbcb327f537b379959c95c4
alexey.berdjugin <alexey.berdjugin@clearscale.net>
2016-08-09 10:29:47 +0700

    Cookbooks commit

---
+++ wordpress/metadata.rb
@@ -0,0 +1,7 @@
+name             'wordpress'
+maintainer       'Clearscale'
+maintainer_email ''
+license          'All rights reserved'
+description      'Configures wordpress'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
