525dacd0fd8b58be8b0bfa81e9e200f6d9e7c9e5
aberdjugin-clearscale <alexey.berdjugin@clearscale.net>
2016-09-07 18:20:33 +0700

    Update default.rb

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -2,8 +2,9 @@
 #
 
 # Enable the Wordpress W3 Total Cache plugin (http://wordpress.org/plugins/w3-total-cache/)?
-default['wordpress']['wp_config']['enable_W3TC'] = false
+default['wordpress']['wp_config']['enable_W3TC'] = 'false'
 
 # Force logins via https (http://codex.wordpress.org/Administration_Over_SSL#To_Force_SSL_Logins_and_SSL_Admin_Access)
-default['wordpress']['wp_config']['force_secure_logins'] = false
+default['wordpress']['wp_config']['force_secure_logins'] = 'false'
 default['opsworks']['ossec']['serverip'] = '172.31.27.100'
+default['opsworks']['ossec']['instancetype'] = 'webapp'
