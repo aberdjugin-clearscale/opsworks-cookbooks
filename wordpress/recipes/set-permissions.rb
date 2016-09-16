ce6e5ad50e20b14c1cd06ae20150ae3c6213b0d6
aberdjugin-clearscale <alexey.berdjugin@clearscale.net>
2016-09-16 15:45:14 +0700

    Update set-permissions.rb

--- wordpress/recipes/set-permissions.rb
+++ wordpress/recipes/set-permissions.rb
@@ -5,9 +5,9 @@
 ##
 ## All rights reserved - Do Not Redistribute
 ##
-#
+#command "chown -R www-data:www-data #{node[:deploy][:nimblestorage][:deploy_to]}/current"
 
 execute 'set_permissions' do
-	command "chown -R www-data:www-data #{node[:deploy][:nimblestorage][:deploy_to]}/current"
+	command "chown -R www-data:www-data /srv/www/nimblestorage"
 	action :run
 end
