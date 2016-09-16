652b640292372be5046393d98b30a260769dc542
aberdjugin-clearscale <alexey.berdjugin@clearscale.net>
2016-09-16 15:20:29 +0700

    Add files via upload

---
+++ wordpress/recipes/set-permissions.rb
@@ -0,0 +1,13 @@
+## Cookbook Name::
+## Recipe:: wordpress
+##
+## Copyright 2015, ClearScale, Inc.
+##
+## All rights reserved - Do Not Redistribute
+##
+#
+
+execute set_permissions do
+	command "chown -R www-data:www-data #{node[:deploy][:nimblestorage][:deploy_to]}/current"
+	action :run
+end
