b92ec563a5b28d3688b4114856988e60e0262456
aberdjugin-clearscale <alexey.berdjugin@clearscale.net>
2016-09-23 12:01:08 +0700

    Update memcached.rb

--- wordpress/recipes/memcached.rb
+++ wordpress/recipes/memcached.rb
@@ -10,30 +10,3 @@ cookbook_file '/usr/lib/php5/20121212/amazon-elasticache-cluster-client.so' do
 	mode '0644'
 	action :create
 end
-
-# template '/etc/php5/mods-available/memcached.ini' do
-#	source 'memcached.ini.erb'
-#	owner 'root'
-#	group 'root'
-#	mode '0644'
-#	variables ({
-#     		:cache_srv => "stg-blue-cache.3cwxgj.cfg.usw2.cache.amazonaws.com:11211"
-#    	})
-#    	action :nothing
-#end
-
-#cookbook_file '/etc/php5/mods-available/memcached.ini' do
-#	source 'memcached.ini'
-#	owner 'root'
-#	group 'root'
-#	mode '0644'
-#	action :create
-#end
-
-#link '/etc/php5/mods-available/memcached.ini' do
-#	to '/etc/php5/apache2/conf.d/20-memcached.ini'
-#end
-
-#link '/etc/php5/mods-available/memcached.ini' do
-#	to '/etc/php5/cli/conf.d/20-memcached.ini'
-#end
